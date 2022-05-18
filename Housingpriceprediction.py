88689d534eefdd2153e6d3111f0c3ca273be40f4
enforcerzz <53108888+enforcerzz@users.noreply.github.com>
2022-05-18 13:04:44 +0530

    Update Housingpriceprediction.py

--- Housingpriceprediction.py
+++ Housingpriceprediction.py
@@ -31,30 +31,30 @@ st.sidebar.header('Specify Input Parameters')
 
 def user_input_features():
     CRIM = st.sidebar.slider('Crime rate', float(X.CRIM.min()), float(X.CRIM.max()), float(X.CRIM.mean()))
-    ZN = st.sidebar.slider('Proportion of residential land zoned for lots over 25,000 sq.ft.', float(X.ZN.min()), float(X.ZN.max()), float(X.ZN.mean()))
-    INDUS = st.sidebar.slider('Proportion of non-retail business acres per town (Between 0 to 30)', float(X.INDUS.min()), float(X.INDUS.max()), float(X.INDUS.mean()))
-    CHAS = st.sidebar.slider('Charles River dummy variable (= 1 if tract bounds river; 0 otherwise)', float(X.CHAS.min()), float(X.CHAS.max()), float(X.CHAS.mean()))
-    NOX = st.sidebar.slider('Nitric oxides concentration (Between 0.3 to 0.9%)', float(X.NOX.min()), float(X.NOX.max()), float(X.NOX.mean()))
-    RM = st.sidebar.slider('Number of Rooms', float(X.RM.min()), float(X.RM.max()), float(X.RM.mean()))
-    AGE = st.sidebar.slider('Age of the residence(years old)', float(X.AGE.min()), float(X.AGE.max()), float(X.AGE.mean()))
-    DIS = st.sidebar.slider('Distances to five Boston employment centres', float(X.DIS.min()), float(X.DIS.max()), float(X.DIS.mean()))
-    RAD = st.sidebar.slider('Distance from express Highways', float(X.RAD.min()), float(X.RAD.max()), float(X.RAD.mean()))
-    TAX = st.sidebar.slider('Tax rate ', float(X.TAX.min()), float(X.TAX.max()), float(X.TAX.mean()))
-    PTRATIO = st.sidebar.slider('Pupil Teacher Ratio', float(X.PTRATIO.min()), float(X.PTRATIO.max()), float(X.PTRATIO.mean()))
-    B = st.sidebar.slider('Proportion of blacks by Town', float(X.B.min()), float(X.B.max()), float(X.B.mean()))
-    LSTAT = st.sidebar.slider('% of lower status population', float(X.LSTAT.min()), float(X.LSTAT.max()), float(X.LSTAT.mean()))
-    data = {'Crime rate': CRIM,
-            'residential land zoned': ZN,
-            'Proportion of non-retail business': INDUS,
-            'Proportion of non-retail business': CHAS,
-            'Nitric oxides concentration': NOX,
-            'no. of rooms': RM,
-            'Age of the residence': AGE,
-            'Distances to five Boston employment centre': DIS,
-            'Distance from express Highways': RAD,
-            'Tax rate': TAX,
-            'Pupil Teacher Ratio': PTRATIO,
-            'Proportion of blacks': B,
+    ZN = st.sidebar.slider('ZN', float(X.ZN.min()), float(X.ZN.max()), float(X.ZN.mean()))
+    INDUS = st.sidebar.slider('INDUS', float(X.INDUS.min()), float(X.INDUS.max()), float(X.INDUS.mean()))
+    CHAS = st.sidebar.slider('CHAS', float(X.CHAS.min()), float(X.CHAS.max()), float(X.CHAS.mean()))
+    NOX = st.sidebar.slider('NOX', float(X.NOX.min()), float(X.NOX.max()), float(X.NOX.mean()))
+    RM = st.sidebar.slider('Number of rooms', float(X.RM.min()), float(X.RM.max()), float(X.RM.mean()))
+    AGE = st.sidebar.slider('AGE', float(X.AGE.min()), float(X.AGE.max()), float(X.AGE.mean()))
+    DIS = st.sidebar.slider('DIS', float(X.DIS.min()), float(X.DIS.max()), float(X.DIS.mean()))
+    RAD = st.sidebar.slider('RAD', float(X.RAD.min()), float(X.RAD.max()), float(X.RAD.mean()))
+    TAX = st.sidebar.slider('TAX', float(X.TAX.min()), float(X.TAX.max()), float(X.TAX.mean()))
+    PTRATIO = st.sidebar.slider('PTRATIO', float(X.PTRATIO.min()), float(X.PTRATIO.max()), float(X.PTRATIO.mean()))
+    B = st.sidebar.slider('B', float(X.B.min()), float(X.B.max()), float(X.B.mean()))
+    LSTAT = st.sidebar.slider('LSTAT', float(X.LSTAT.min()), float(X.LSTAT.max()), float(X.LSTAT.mean()))
+    data = {'CRIM': CRIM,
+            'ZN': ZN,
+            'INDUS': INDUS,
+            'CHAS': CHAS,
+            'NOX': NOX,
+            'Number of Rooms': RM,
+            'AGE': AGE,
+            'DIS': DIS,
+            'RAD': RAD,
+            'TAX': TAX,
+            'PTRATIO': PTRATIO,
+            'B': B,
             '% of lower status population': LSTAT}
     features = pd.DataFrame(data, index=[0])
     return features
@@ -74,7 +74,7 @@ model.fit(X, Y)
 # Apply Model to Make Prediction
 prediction = model.predict(df)
 
-st.header('Prediction of Selling Price')
+st.header('Prediction of selling price')
 st.write(prediction)
 st.write('---')
 
@@ -85,14 +85,10 @@ shap_values = explainer.shap_values(X)
 
 st.header('Feature Importance')
 plt.title('Feature importance based on SHAP values')
-#shap.summary_plot(shap_values, X)
-#st.pyplot(bbox_inches='tight')
+shap.summary_plot(shap_values, X)
+st.pyplot(bbox_inches='tight')
 st.write('---')
 
 plt.title('Feature importance based on SHAP values (Bar)')
 shap.summary_plot(shap_values, X, plot_type="bar")
-st.pyplot(bbox_inches='tight')
-
-plt.title('Feature importance based on SHAP values (violin)')
-shap.summary_plot(shap_values,X,plot_type='violin')
 st.pyplot(bbox_inches='tight')
